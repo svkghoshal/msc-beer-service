6c586e6dd7373cf02511d9c9c65c2e6dfd5a653d
Souvik Ghoshal <svkghoshal@gmail.com.com>
2020-09-28 18:09:03 +0530

    initial project add. closes #1

---
+++ src/main/java/guru/springframework/mscbeerservice/MscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/mscbeerservice/MscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.mscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
